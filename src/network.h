272a1ffdd5a6744d43977a68153c59e0ccf95f2a
jcoppens <jcoppens@ghgsat.com>
2020-01-31 14:40:51 -0500

    Adds Network class

--- src/network.h
+++ src/network.h
@@ -1,7 +1,27 @@
 // 'network.cpp' SRC: J.Coppens 2020
 
+#ifndef NETWORK
+#define NETWORK
 
-int main(){
+#include <random>
 
-    return 0;
-}
+class Network {
+    // Data
+    public:
+        int num_layers;
+        int *sizes;
+        float *biases;
+        float *weights;
+
+    // Methods
+    public: 
+        Network(int *layer_sizes, int N) : num_layers(N), sizes(layer_sizes) {
+            
+            std::random_device rd;
+            std::mt19937 generator(rd());
+            std::normal_distribution<float> inital_value(0,1);
+        }
+
+};
+
+#endif
