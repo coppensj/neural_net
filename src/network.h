c43df4ce15fc3393bcfea8cd89b4def2386d36d1
jcoppens <jcoppens@ghgsat.com>
2020-01-31 15:52:14 -0500

    Adds initialization of bias nodes.  Renames executable.

--- src/network.h
+++ src/network.h
@@ -1,17 +1,18 @@
-// 'network.cpp' SRC: J.Coppens 2020
+// 'network.h' SRC: J.Coppens 2020
 
 #ifndef NETWORK
 #define NETWORK
 
 #include <random>
+#include <iostream>
 
 class Network {
     // Data
     public:
         int num_layers;
         int *sizes;
-        float *biases;
-        float *weights;
+        std::vector<std::vector<float>> biases;
+        std::vector<std::vector<std::vector<float>>> weights;
 
     // Methods
     public: 
@@ -19,9 +20,18 @@ class Network {
             
             std::random_device rd;
             std::mt19937 generator(rd());
-            std::normal_distribution<float> inital_value(0,1);
-        }
+            std::normal_distribution<float> initial_value(0,1);
 
+            biases.reserve(num_layers - 1);
+            for(int i=1; i<num_layers; i++){
+                std::cout << "Layer " << i << " Biases:\n";
+                biases[i-1].reserve(sizes[i]);
+                for(int j=0; j<sizes[i]; j++){
+                    biases[i-1][j] = initial_value(generator);
+                    std::cout << "    " << biases[i-1][j] << std::endl;
+                }
+            }
+        }
 };
 
 #endif
