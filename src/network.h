6684f97d415e4667a874c95ea9b8018825826043
jcoppens <jcoppens@ghgsat.com>
2020-02-03 15:59:44 -0500

    Changes sigmoid to accept arrays instead of single values

--- src/network.h
+++ src/network.h
@@ -8,8 +8,11 @@
 
 #include <iostream>
 
-float sigmoid(float z){
-    return 1.0/(1.0 + exp(-z));
+std::vector<float> sigmoid(std::vector<float> &z){
+    std::vector<float> z_new(z.size());
+    for(int i=0; i<z.size(); i++)
+        z_new[i] = 1.0/(1.0 + exp(-z[i]));
+    return z_new;
 }
 
 class Network {
@@ -29,25 +32,29 @@ class Network {
             std::normal_distribution<float> initial_value(0,1);
 
             // Initialize biases
-            biases.reserve(num_layers - 1);
+            biases.resize(num_layers - 1);
             for(int i=0; i<num_layers-1; i++){
-                biases[i].reserve(sizes[i+1]);
-                for(int j=0; j<sizes[i+1]; j++)
+                biases[i].resize(sizes[i+1]);
+                for(int j=0; j<sizes[i+1]; j++){
                     biases[i][j] = initial_value(generator);
+                }
             }
            
             // Initialize weights
-            weights.reserve(num_layers - 1);
+            weights.resize(num_layers - 1);
             for(int i=0; i<num_layers-1; i++){
-                weights[i].reserve(sizes[i+1]);
+                weights[i].resize(sizes[i+1]);
                 
                 for(int row=0; row<sizes[i+1]; row++){    
-                    weights[i][row].reserve(sizes[i]);
+                    weights[i][row].resize(sizes[i]);
                     for(int col=0; col<sizes[i]; col++)
                         weights[i][row][col] = initial_value(generator);
                 }
             }
-        
+            
+            std::vector<float> test = sigmoid(biases[0]);
+            for(int i=0; i<test.size(); i++)
+                std::cout << biases[0][i] << " " << test[i] << std::endl;
         }
 };
 
